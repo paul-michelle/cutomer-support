dca2af11e0ce36e8d901f70170f9493177751138
paul-michelle <paulmichellegallery@gmail.com>
2022-07-07 21:41:11 +0300

    Golang SQL. Initial. Establish conn to postges.

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "database/sql"
+    "fmt"
+    "log"
+	"os"
+    _ "github.com/lib/pq"
+)
+
+var (
+    HOST 		= os.Getenv("DB_HOST")
+    PORT 		= os.Getenv("DB_PORT")
+	USERNAME 	= os.Getenv("DB_USER")
+	PASSWORD 	= os.Getenv("DB_PASSWORD")
+	DATABASE	= os.Getenv("DB_NAME")
+)
+
+type Database struct {
+    Conn *sql.DB
+}
+
+func Initialize(username, password, database string) (Database, error) {
+    db 	:= 	Database{}
+    dsn := 	fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+			HOST, PORT, USERNAME, PASSWORD, DATABASE)
+    conn, err := sql.Open("postgres", dsn)
+    if err != nil { return db, err }
+
+    db.Conn = conn
+    err = db.Conn.Ping()
+    if err != nil { return db, err }
+	
+    log.Println("Database connection established")
+    return db, nil
+}
+
+func main () {
+	db, err := Initialize(	os.Getenv("username"), 
+							os.Getenv("password"), 
+							os.Getenv("database"))
+	if err != nil { log.Fatal(err) }
+	fmt.Println(db)
+}
